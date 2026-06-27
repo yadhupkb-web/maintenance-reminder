334a95fef8eb15147560bfe68485e05422a941e1
Yadhu <yadhupkb@gmail.com>
2026-06-27 17:22:34 +0530

    Fix browser crash by removing single-process flag

--- server/index.js
+++ server/index.js
@@ -70,8 +70,6 @@ const client = new Client({
       '--disable-dev-shm-usage',
       '--disable-accelerated-2d-canvas',
       '--no-first-run',
-      '--no-zygote',
-      '--single-process',
       '--disable-gpu'
     ],
   },
